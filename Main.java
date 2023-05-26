8754b7769b3552e25ad46bf813da6737b1f19ae7
Davi Lima <84492696+DaviLim@users.noreply.github.com>
2023-05-26 12:06:17 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import org.hibernate.Session;
 
-ublic class Main {
+public class Main {
 	public static void main(String[] args) {
 		HibernateConfiguration hbCfg = new HibernateConfiguration();
 		
